818a534ce1445de54fe2d6638d66f92c478d4eea
Jorge Huerta <jorgelhuerta1020@gmail.com>
2026-07-02 18:37:15 -0500

    feat: implement header hamburger menu toggle

--- front-end/src/js/index.js
+++ front-end/src/js/index.js
@@ -1,1 +1,29 @@
-console.log("Hello World");
+const menuToggle = document.getElementById("menu-toggle");
+const mobileNav = document.getElementById("mobile-nav");
+const iconMenuOpen = document.getElementById("icon-menu-open");
+const iconMenuClose = document.getElementById("icon-menu-close");
+
+function closeMobileNav() {
+  mobileNav.classList.add("hidden");
+  mobileNav.classList.remove("flex");
+  iconMenuOpen.classList.remove("hidden");
+  iconMenuClose.classList.add("hidden");
+  menuToggle.setAttribute("aria-expanded", "false");
+}
+
+function openMobileNav() {
+  mobileNav.classList.remove("hidden");
+  mobileNav.classList.add("flex");
+  iconMenuOpen.classList.add("hidden");
+  iconMenuClose.classList.remove("hidden");
+  menuToggle.setAttribute("aria-expanded", "true");
+}
+
+menuToggle.addEventListener("click", () => {
+  const isOpen = menuToggle.getAttribute("aria-expanded") === "true";
+  isOpen ? closeMobileNav() : openMobileNav();
+});
+
+mobileNav.querySelectorAll("a").forEach((link) => {
+  link.addEventListener("click", closeMobileNav);
+});
